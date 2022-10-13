c5653797c114aa433aad926268e634704483427d
Tomás Leggire <tomasleggire@hotmail.com>
2022-10-12 21:35:32 -0300

    Estilos beta terminados

--- src/Components/Dates2.js
+++ src/Components/Dates2.js
@@ -6,24 +6,22 @@ export default function Dates2({resApi}) {
 
     return (
         <div style={divStyle}>
-            <div style={pStyle}>
-                <img style={imgStyle} src="https://cdn.icon-icons.com/icons2/571/PNG/512/wind-weather-lines-group-symbol_icon-icons.com_54629.png" />
+            <div style={divStyle2}>
                 <span style={spanStyle}>Feels Like</span>
-                <span style={porcentaje}>{Math.round(resApi.main.feels_like) + 'º'}</span>
-            </div>
-            <div style={pStyle}>
-                <img style={imgStyle} src="https://cdn.icon-icons.com/icons2/3406/PNG/512/humidity_icon_216457.png" />
                 <span style={spanStyle}>Humidity</span>
-                <span style={porcentaje}>{resApi.main.humidity + "%"}</span>
-            </div>
-            <div style={pStyle}>
-                <img style={imgStyle} src="https://cdn.icon-icons.com/icons2/38/PNG/512/pressure_4667.png" />
                 <span style={spanStyle}>Pressure</span>
-                <span style={porcentaje}>{resApi.main.pressure}</span>
-            </div>
-            <div style={pStyle}>
-                <img style={imgStyle} src="https://cdn.icon-icons.com/icons2/565/PNG/512/turbine-wind_icon-icons.com_54230.png" />
                 <span style={spanStyle}>Wind Speed</span>
+            </div>
+            <div style={divStyle3}>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png"/>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png"/>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png"/>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png"/>
+            </div>
+            <div style={divStyle4}>
+                <span style={porcentaje}>{Math.round(resApi.main.feels_like) + 'º'}</span>
+                <span style={porcentaje}>{resApi.main.humidity + "%"}</span>
+                <span style={porcentaje}>{resApi.main.pressure}</span>
                 <span style={porcentaje}>{resApi.wind.speed}</span>
             </div>
         </div>
@@ -32,28 +30,67 @@ export default function Dates2({resApi}) {
 
 const divStyle = {
     width: '280px',
-    backgroundColor: 'green',
+    paddingBottom: '20px',
+    display: 'flex',
+    justifyContent: 'space-between',
 }
 
-const pStyle = {
+const divStyle2 = {
     display: 'flex',
-    alignItems: 'center',
-    color: '#d9d9d9',
-    fontWeight: 'bold',
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'space-around',
+    color: 'black',
+}
+
+const divStyle3 = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    paddingRight: '20px',
+}
+
+const divStyle4 = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
 }
 
 const spanStyle = {
     fontSize: '18px',
-    fontFamily: 'monospace',
-    paddingLeft: '10px',
+    
 }
 
 const imgStyle = {
     width: '25px',
     height: '25px',
+    margin: '0 0 0 auto',
 }
 
 const porcentaje = {
-    margin: '0 0 0 auto',
-    paddingRight: '10px',
+    textAlign: 'right',
 }
+
+
+{/* <div style={divStyle2}>
+                <span style={spanStyle}>Feels Like</span>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png"/>
+                <span style={porcentaje}>{Math.round(resApi.main.feels_like) + 'º'}</span>
+             </div>
+            <div style={divStyle2}>
+                <span style={spanStyle}>Humidity</span>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png" />
+                <span style={porcentaje}>{resApi.main.humidity + "%"}</span>
+             </div>
+           <div style={divStyle2}>
+                
+                <span style={spanStyle}>Pressure</span>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png" />
+                <span style={porcentaje}>{resApi.main.pressure}</span>
+            </div>
+            <div style={divStyle2}>
+                
+                <span style={spanStyle}>Wind Speed</span>
+                <img style={imgStyle} src="https://cdn-icons-png.flaticon.com/512/2078/2078262.png" />
+                <span style={porcentaje}>{resApi.wind.speed}</span>
+            </div>  */}
